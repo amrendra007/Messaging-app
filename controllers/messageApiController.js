b8ca9d1e1c45131c846eedf323c6d82f5f301364
amrendra007 <amrendrapcs07@gmail.com>
2018-08-15 00:50:21 +0530

    Setup message Api file

---
+++ controllers/messageApiController.js
@@ -0,0 +1,7 @@
+const express = require('express');
+// const { check, validationResult } = require('express-validator/check');
+// const { sanitizeBody } = require('express-validator/filter');
+
+const router = express.Router();
+
+module.exports = router;
