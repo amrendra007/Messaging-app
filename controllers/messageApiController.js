a53e4f023a438826a5a361c0dd6cb4375b9ad280
amrendra007 <amrendrapcs07@gmail.com>
2018-08-15 02:47:27 +0530

    Message api working now

--- controllers/messageApiController.js
+++ controllers/messageApiController.js
@@ -3,10 +3,12 @@ const { check, validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 
 const router = express.Router();
+const User = require('../models/userDb');
+const Message = require('../models/messageDb');
 
 const validator = [
     check('subject').trim().exists()
-        .withMessage('Message subject shouldn\'t be blank'),
+        .withMessage('Subject shouldn\'t be blank'),
 
     check('message').trim().exists()
         .withMessage('Message shouldn\'t be blank'),
@@ -25,6 +27,32 @@ router.post('/sendmessage', validator, (req, res, next) => {
         console.log('errors', errors.array());
         return res.status(422).send(errors.array());
     }
+    User.findOne({ username: req.body.toUser }, (error, foundUser) => {
+        if (error) {
+            return next(error);
+        }
+        if (!foundUser) {
+            return res.status(404).send('toUser not found');
+        }
+        const oneMessage = {
+            subject: req.body.subject,
+            message: req.body.message,
+            toUser: req.body.toUser,
+        };
+        Message.create(oneMessage, (err, newMesage) => {
+            if (err) {
+                return next(err);
+            }
+            foundUser.messages.unshift(newMesage);
+            foundUser.save((er) => {
+                if (er) {
+                    return next(er);
+                }
+                console.log('newMEssage', newMesage);
+                res.status(200).send(`your message successfully sent to ${foundUser.username}`);
+            });
+        });
+    });
 });
 
 module.exports = router;
