07c9312b484badf306c4058ae158ccae6b6ccee4
amrendra007 <amrendrapcs07@gmail.com>
2018-08-14 19:53:32 +0530

    Add database model

---
+++ models/userDb.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('User', UserSchema);
